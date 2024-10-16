4b36e739604ad395ece855b584de14c319801d8b
kmoler1 <kmoler1@uwyo.edu>
2024-10-16 21:12:51 +0000

    least

--- cosc1010-lab06.py
+++ cosc1010-lab06.py
@@ -105,7 +105,7 @@ print("*"*75)
 print(f"The letter that occurred the most is {most_occurred}")
 print("*"*75)
 # Output which letter occurred the least 
-print(f"The letter that occurred the most is {least_occurred}")
+print(f"The letter that occurred the least is {least_occurred}")
 print("*"*75)
 
 # Output what the percentage of the string each character is, again in alphabetical
